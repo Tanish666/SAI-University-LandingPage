89d871b441b70c8187304f97c0533c327d6b93e5
Tanish Saxena <saxenakaju7@gmail.com>
2026-03-11 12:49:22 +0530

    font related issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto_Flex } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const robotoFlex = Roboto_Flex({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-roboto-flex",
 });
 
 export const metadata = {
@@ -20,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={robotoFlex.className}>
         {children}
       </body>
     </html>
